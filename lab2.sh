acb32417bbca772b063648458e48f7c3bf54c089
LoSk-p <Bo5989279598>
2019-10-06 23:09:40 +0300

    Lab2

--- lab2.sh
+++ lab2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 wget "https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz"
-tar -C /usr/local -xzf go1.13.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.13.1.linux-amd64.tar.gz
+export GOPATH=~/go
 export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$GOPATH/bin
 source $HOME/.profile
+sudo apt install git
 git clone https://github.com/yggdrasil-network/yggdrasil-go.git
 cd yggdrasil-go
 ./build
